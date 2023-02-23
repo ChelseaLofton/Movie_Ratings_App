3c22ef6405aa296d32aaaae39bee6990c23c5ece
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-02-22 17:39:23 -0800

    All

---
+++ crud.py
@@ -0,0 +1,33 @@
+""" CRUD Operations"""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+    
+    return user
+
+def create_movie(title, overview, release_date, poster_path):
+    movie = Movie(
+        title = title,
+        overview = overview,
+        release_date = release_date,
+        poster_path = poster_path,
+    )
+
+    return movie
+
+def create_rating(user, movie, rating):
+    rating = Rating(
+        user = user,
+        movie = movie,
+        rating = rating
+    )
+
+    return rating
+
+if __name__ == "main":
+    from server import app
+    connect_to_db(app, echo=False)
---
+++ model.py
@@ -0,0 +1,75 @@
+"""Models for movie ratings app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# Replace this with your code!
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+class User(db.Model):
+
+    __tablename__ = "user"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="user")
+
+
+    def __repr__(self):
+        return f"<User_id = {self.user_id} and email = {self.email}>"
+
+
+class Movie(db.Model):
+
+    __tablename__ = "movie"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="movie")
+
+    def __repr__(self):
+        return f"<Movie_id = {self.movie_id} and title = {self.title}>"
+
+
+class Rating(db.Model):
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rating = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+
+    def __repr__(self):
+        return f"<Rating_id= {self.rating_id} and score = {self.rating}>"
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
