50c6b67c3306586619321d411abf6b781b364907
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-02-24 13:05:50 -0800

    Completed

--- crud.py
+++ crud.py
@@ -32,6 +32,26 @@ def get_all_movies():
     
     return Movie.query.all()
 
+def get_movie_by_id(movie_id):
+    
+    movie_by_id = Movie.query.get(movie_id)
+    return movie_by_id
+
+def get_all_users():
+    
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    
+    user_by_id = User.query.get(user_id)
+    return user_by_id
+
+def get_user_by_email(email):
+
+    return User.query.filter(User.email == email).first() 
+
 if __name__ == "main":
     from server import app
     connect_to_db(app, echo=False)
+
+
--- server.py
+++ server.py
@@ -22,13 +22,64 @@ def view_all_movies():
 
     return render_template("all_movies.html", movies=movies)
 
-@app.route("/movies/<movie_title>")
-def chosen_movie():
+@app.route("/movies/<movie_id>")
+def chosen_movie(movie_id):
 
-    movie = request.args['title']
+    movie = crud.get_movie_by_id(movie_id)
 
-    return render_template("chosen_movies.html", movie=movie)
+    return render_template("movie_details.html", movie=movie)
 
+
+@app.route("/users")
+def view_all_users():
+
+    users = crud.get_all_users()
+
+    return render_template("all_user.html", users=users )
+
+@app.route("/users", methods = ["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("This email already exits, please log in!")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Success! Your account has been created!")
+
+    return redirect ("/")
+
+
+@app.route("/login", methods = ["POST"])
+def login_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if not user:
+        flash("Please create an account")
+
+    elif password != user.password:
+        flash("Incorrect password!")
+        
+    else:
+        session['user_id'] = user.user_id
+        flash("Successfully logged in!")
+
+    return redirect ("/")
+
+@app.route("/users/<user_id>")
+def chosen_user(user_id):
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
 
 
 if __name__ == "__main__":
